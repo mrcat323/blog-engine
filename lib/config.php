50eaac359e8800bcc689a3abf1c68cd100d506dc
Mr CaT <mrcat323@gmail.com>
2017-05-03 18:27:34 +0500

    The CMS had been renamed

--- lib/config.php
+++ lib/config.php
@@ -1,13 +1,12 @@
 <?php
-	ini_set("error_reporting", 1);
-	ini_set("display_errors", 1);
 	set_time_limit(0);
-	define("HOST", "mysql:host=localhost; dbname=hasanov");
+	define("HOST", "mysql:host=; dbname=");
 	define("USER", "root");
 	define("PWD", "jojo");
 	define("HOME_NUM_ARTICLES", 5);
 	define("ADDRESS", "http://localhost/");
-	define("NAME", "CAT ENGINE");
+	define("NAME", "BLOG-ENGINE");
+	define("OWNER", "");
 	try {
     $dbh = new PDO(HOST, USER, PWD);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
--- page.php
+++ page.php
@@ -22,7 +22,7 @@
 					<span class="icon-bar"></span>
 					<span class="icon-bar"></span>
 				</button>
-				<a class="navbar-brand" href="<?php echo ADDRESS;?>">CAT ENGINE</a>
+				<a class="navbar-brand" href="<?php echo OWNER . ADDRESS;?>">CAT ENGINE</a>
 			</div>
 	
 			<!-- Collect the nav links, forms, and other content for toggling -->
