f3df9fcbc08518558275df98462fc04ecf7e9e0a
Mr. CaT <mrcat323@gmail.com>
2019-07-10 21:41:59 +0500

    Configs

---
+++ src/config.php
@@ -0,0 +1,18 @@
+<?php 
+
+return [
+	'settings' => [
+		'displayErrorDetails' => true,
+		'addContentLengthHeader' => false,
+		'db' => [
+			'driver' => 'mysql',
+	        'host' => 'localhost',
+	        'database' => 'blog_engine',
+	        'username' => 'root',
+	        'password' => 'jojo',
+	        'charset'   => 'utf8',
+	        'collation' => 'utf8_unicode_ci',
+	        'prefix'    => ''
+		]
+	]
+];
