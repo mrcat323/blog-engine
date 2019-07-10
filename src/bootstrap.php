d64ff381b6f7c54a3f3658953e6418feceb26fe8
Mr. CaT <mrcat323@gmail.com>
2019-07-10 21:46:32 +0500

    Logger & Twig View as dependencies

--- src/bootstrap.php
+++ src/bootstrap.php
@@ -22,3 +22,26 @@ $container['db'] = function ($c) use ($capsule) {
 
 // finishing ...
 
+// Another components
+
+$container['logger'] = function($c) {
+	$logger = new \Monolog\Logger('my-logger');
+	$fileHandler = new \Monolog\Handler\StreamHandler('../logs/app.log');
+	$logger->pushHandler($fileHandler);
+
+	return $logger;
+};
+
+$container['view'] = function ($container) {
+    $view = new \Slim\Views\Twig('../templates', [
+        'cache' => false
+    ]);
+
+    // Instantiate and add Slim specific extension
+    $router = $container->get('router');
+    $uri = \Slim\Http\Uri::createFromEnvironment(new \Slim\Http\Environment($_SERVER));
+    $view->addExtension(new \Slim\Views\TwigExtension($router, $uri));
+
+    return $view;
+};
+
